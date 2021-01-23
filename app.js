24d7c3bd7454c47668e4faab6fc537421045cfd7
unknown <subhamsbharadwaz@gmail.com>
2021-01-23 09:12:59 +0530

    debug  css

---
+++ app.js
@@ -0,0 +1,12 @@
+const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener('click', () => highlightCups(idx));
+});
+
+function highlightCups(idx) {
+  console.log(idx);
+}
